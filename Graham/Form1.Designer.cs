44aab684d21e2e6934836aec827d9fcf70739e0a
Geluos <55185142+Geluos@users.noreply.github.com>
2022-11-02 16:49:14 +0300

    Add: Graham algorithm

--- Graham/Form1.Designer.cs
+++ Graham/Form1.Designer.cs
@@ -28,20 +28,74 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.ClearButton = new System.Windows.Forms.Button();
+			this.ExecButton = new System.Windows.Forms.Button();
+			this.StepButton = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(776, 396);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+			// 
+			// ClearButton
+			// 
+			this.ClearButton.Location = new System.Drawing.Point(163, 415);
+			this.ClearButton.Name = "ClearButton";
+			this.ClearButton.Size = new System.Drawing.Size(75, 23);
+			this.ClearButton.TabIndex = 1;
+			this.ClearButton.Text = "Очистить";
+			this.ClearButton.UseVisualStyleBackColor = true;
+			this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+			// 
+			// ExecButton
+			// 
+			this.ExecButton.Location = new System.Drawing.Point(343, 415);
+			this.ExecButton.Name = "ExecButton";
+			this.ExecButton.Size = new System.Drawing.Size(75, 23);
+			this.ExecButton.TabIndex = 2;
+			this.ExecButton.Text = "Запустить";
+			this.ExecButton.UseVisualStyleBackColor = true;
+			this.ExecButton.Click += new System.EventHandler(this.ExecButton_Click);
+			// 
+			// StepButton
+			// 
+			this.StepButton.Location = new System.Drawing.Point(531, 415);
+			this.StepButton.Name = "StepButton";
+			this.StepButton.Size = new System.Drawing.Size(75, 23);
+			this.StepButton.TabIndex = 3;
+			this.StepButton.Text = "Шаг+";
+			this.StepButton.UseVisualStyleBackColor = true;
+			this.StepButton.Click += new System.EventHandler(this.StepButton_Click);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.StepButton);
+			this.Controls.Add(this.ExecButton);
+			this.Controls.Add(this.ClearButton);
+			this.Controls.Add(this.pictureBox1);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
+
+		private PictureBox pictureBox1;
+		private Button ClearButton;
+		private Button ExecButton;
+		private Button StepButton;
 	}
 }
--- Graham/Form1.cs
+++ Graham/Form1.cs
@@ -1,15 +1,187 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+using System.Drawing.Drawing2D;
+
 namespace Graham
 {
 	public partial class Form1 : Form
 	{
+		Graphics g;
+		List<Point> bPoints = new List<Point>();
+
+		int current = 0;
+		List<Point> gPoints;
+		Stack<Point> stack = new Stack<Point>();
+
 		public Form1()
 		{
 			InitializeComponent();
+			pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+			g = Graphics.FromImage(pictureBox1.Image);
 		}
 
 		private void Form1_Load(object sender, EventArgs e)
 		{
 
+		}
+
+		private void CreatePoint(Point x)
+		{
+			bPoints.Add(x);
+			DrawPoint(x, Color.Black);
+		}
+
+		private void DrawPoint(Point x,Color color)
+		{
+			Pen pen = new Pen(color);
+			g.DrawEllipse(pen, x.X - 1, x.Y - 1, 3, 3);
+			pictureBox1.Invalidate();
+		}
+
+		private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+		{
+			CreatePoint(e.Location);
+		}
+
+		private void ExecButton_Click(object sender, EventArgs e)
+		{
+			if (bPoints.Count < 3)
+				return;
+
+			g.Clear(pictureBox1.BackColor);
+
+			gPoints = new List<Point>();
+
+			Point mn = bPoints.Min(new PointComparer());
+			gPoints.Add(mn);
+
+			foreach (Point p in bPoints)
+			{
+				if (p != mn)
+				{
+					DrawPoint(p, Color.Black);
+					gPoints.Add(p);
+				}
+			}
+
+			DrawPoint(mn, Color.Red);
+
+			gPoints.Sort(1, gPoints.Count-1, new PointRotateComparer(mn));
+
+			current = 2;
+
+			stack.Clear();
+
+			stack.Push(gPoints[0]);
+			stack.Push(gPoints[1]);
+
+			Stepf();
+			
+		}
+
+		
+
+		private void Stepf()
+		{
+			if (current >= gPoints.Count)
+				return;
+
+			//!!REWRITE
+			while (Rotate(stack.ElementAt(1), stack.Peek(), gPoints[current]) > 0)
+			{
+				stack.Pop();
+			}
+
+			stack.Push(gPoints[current]);
+			++current;
+
+			g.Clear(pictureBox1.BackColor);
+
+			foreach (Point p in gPoints)
+				DrawPoint(p, Color.Black);
+
+			g.DrawLine(new Pen(Color.Black), gPoints[0], stack.Peek());
+			g.DrawLine(new Pen(Color.Black), gPoints[0], stack.Last());
+
+			for (int i = 1; i < stack.Count; ++i)
+			{
+				g.DrawLine(new Pen(Color.Black), stack.ElementAt(i), stack.ElementAt(i - 1));
+			}
+
+			pictureBox1.Invalidate();
+		}
+
+
+		class PointComparer : IComparer<Point>
+		{
+			public int Compare(Point a, Point b)
+			{
+				if (a.X < b.X)
+					return -1;
+				if (a.X > b.X)
+					return 1;
+				if (a.Y < b.Y)
+					return -1;
+				if (a.Y > b.Y)
+					return 1;
+				return 0;
+			}
+		}
+
+		public static float Rotate(Point A, Point B, Point C)
+		{
+			return (B.X - A.X) * (C.Y - B.Y) - (B.Y - A.Y) * (C.X - B.X);
+		}
+
+		class PointRotateComparer : IComparer<Point>
+		{
+			private Point rPoint;
+			public PointRotateComparer(Point x)
+			{
+				rPoint = x;
+			}
+
+			public int Compare(Point a, Point b)
+			{
+				float r1 = Rotate(rPoint, a, b);
+				float r2 = Rotate(rPoint, b, a);
+
+				if (r1 < r2)
+				{
+					return -1;
+				}
+				if (r1 > r2)
+				{
+					return 1;
+				}
+				return 0;
+			}
+		}
+
+		void Clear()
+		{
+			g.Clear(pictureBox1.BackColor);
+			gPoints.Clear();
+			bPoints.Clear();
+		}
+
+		private void StepButton_Click(object sender, EventArgs e)
+		{
+			Stepf();
+		}
+
+		private void ClearButton_Click(object sender, EventArgs e)
+		{
+			Clear();
 		}
 	}
 }
